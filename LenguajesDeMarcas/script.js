db6aa307d70f1f40c87b84def8394cdbc2d30f1d
David-Dama <davidlopezzurron@gmail.com>
2026-02-24 12:10:33 +0100

    retoques pagina web

--- LenguajesDeMarcas/script.js
+++ LenguajesDeMarcas/script.js
@@ -44,3 +44,23 @@ prevBtn.addEventListener("click", () => {
 
 // Inicializar
 updateCarousel(current);
+
+//Formulario
+const formulario = document.querySelector(".formulario");
+
+formulario.addEventListener("submit", function(e) {
+  e.preventDefault(); // Evita que la página se recargue
+
+  const nombre = document.getElementById("nombre").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const mensaje = document.getElementById("mensaje").value.trim();
+
+  if (nombre === "" || email === "" || mensaje === "") {
+    alert("Por favor, completa todos los campos.");
+    return;
+  }
+
+  alert("Formulario enviado correctamente");
+
+  formulario.reset(); // Limpia el formulario
+});
