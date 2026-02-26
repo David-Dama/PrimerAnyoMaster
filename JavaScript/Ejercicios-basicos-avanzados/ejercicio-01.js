595b71457b48616abd9933085ae1ba0eecb11d28
David-Dama <davidlopezzurron@gmail.com>
2026-02-26 11:08:38 +0100

    Plantillas ejercicios javaScript y eliminacion de carpetas de prettier de la carpetas lenguaje de marcas

---
+++ JavaScript/Ejercicios-basicos-avanzados/ejercicio-01.js
@@ -0,0 +1,35 @@
+/*1. Define tu superhéroe favorito:
+
+Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk".
+
+2. Define un número:
+
+Crea una variable llamada numeroFavorito y asigna el valor 50.
+
+3. Define las dimensiones:
+
+Crea una variable llamada altura y asigna el valor 5.
+
+Crea una variable llamada longitud y asigna el valor 10.
+
+4. Calcula la suma:
+
+Crea una variable llamada suma y asigna el valor de altura + longitud.
+
+5. Imprime los resultados:
+
+Muestra todas las variables por consola.*/
+
+let miSuperheroeFavorito = "Hulk";
+
+let numeroFavorito = 50;
+
+let altura = 5;
+
+let longitud = 10;
+
+let suma = altura + longitud;
+
+console.log(
+  `Superheroe favorito: ${miSuperheroeFavorito}\nNumero favorito: ${numeroFavorito}\nAltura ${altura}\n longitud: ${longitud}\nSuma: ${suma}\n`,
+);
