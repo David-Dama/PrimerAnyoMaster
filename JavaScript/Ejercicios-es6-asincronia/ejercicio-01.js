63ab3bf1c77268c5eda9bb87b029c4440cd8ff63
David-Dama <davidlopezzurron@gmail.com>
2026-03-05 13:40:50 +0100

    Ejercicios 1 y 2 es6+ y asincronia hechos

--- JavaScript/Ejercicios-es6-asincronia/ejercicio-01.js
+++ JavaScript/Ejercicios-es6-asincronia/ejercicio-01.js
@@ -1,14 +1,31 @@
 /*1.1 En base al siguiente javascript, crea variables en base a las propiedades
-del objeto usando object destructuring e imprimelas por consola. Cuidado,
-no hace falta hacer destructuring del array, solo del objeto.*/
+del objeto usando object destructuring e imprimelas por consola.
+
+Cuidado, no hace falta hacer destructuring del array, solo del objeto.*/
 
 const game = {title: 'The Last of Us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
 
-/*1.2 En base al siguiente javascript, usa destructuring para crear 3 variables
+const {title, gender, year} = game;
+
+console.log("===== EJERCICIO 1 =====");
+
+console.log(title);
+console.log(gender);
+console.log(year);
+
+/*1.2 Basándonos en el siguiente javascript, usa destructuring para crear 3 variables
 llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
-imprimelo por consola.*/
+imprímelo por consola.*/
 
 const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+const [fruit1, fruit2, fruit3] = fruits;
+
+console.log("===== EJERCICIO 2 =====");
+
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
 
 /*1.3 En base al siguiente javascript, usa destructuring para crear 2
 variables igualandolo a la función e imprimiendolo por consola.*/
@@ -17,9 +34,25 @@ const animalFunction = () => {
     return {name: 'Bengal Tiger', race: 'Tiger'}
 };
 
+const {name, race} = animalFunction()
+
+console.log("===== EJERCICIO 3 =====");
+
+console.log(name)
+console.log(race)
+
 /*1.4 En base al siguiente javascript, usa destructuring para crear las
 variables name y itv con sus respectivos valores. Posteriormente crea
 3 variables usando igualmente el destructuring para cada uno de los años
 y comprueba que todo esta bien imprimiendolo.*/
 
-const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+const car = {name2: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+const{name2, itv: [anyo1, anyo2, anyo3]} = car;
+
+console.log("===== EJERCICIO 4 =====");
+
+console.log(name2)
+console.log(anyo1)
+console.log(anyo2)
+console.log(anyo3)
